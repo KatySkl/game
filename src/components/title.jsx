f3b991eb8418475b4e0279340b0e16071eba98c4
KatySkl <you@example.com>
2022-06-27 15:30:56 +0600

    create title

---
+++ src/components/title.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Title = () => {
+ 
+    return (
+        <div className="title">
+        <i className="material-icons">help_outline</i>
+        <h4>
+            <b>Уровень 1 из 999</b>
+        </h4>
+        </div>
+    )
+}
